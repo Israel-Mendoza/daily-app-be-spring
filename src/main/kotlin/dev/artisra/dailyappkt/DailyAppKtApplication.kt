9e5aef3d3749f64db8bbb32544c76c2115cc175f
Israel Mendoza <xander_irsl@hotmail.com>
2026-04-27 00:04:36 -0600

    Remove CLI runner

--- src/main/kotlin/dev/artisra/dailyappkt/DailyAppKtApplication.kt
+++ src/main/kotlin/dev/artisra/dailyappkt/DailyAppKtApplication.kt
@@ -1,14 +1,8 @@
 package dev.artisra.dailyappkt
 
-import dev.artisra.dailyappkt.repositories.BlockerRepository
-import dev.artisra.dailyappkt.repositories.NoteRepository
-import dev.artisra.dailyappkt.repositories.SubTaskRepository
-import dev.artisra.dailyappkt.repositories.TaskRepository
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.stereotype.Component
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -16,17 +10,4 @@ class DailyAppKtApplication
 
 fun main(args: Array<String>) {
     runApplication<DailyAppKtApplication>(*args)
-}
-
-@Component
-class MyRunner(
-    private val blockerRepository: BlockerRepository,
-    private val noteRepository: NoteRepository,
-) : CommandLineRunner {
-    override fun run(vararg args: String) {
-        println("Notes:")
-        noteRepository.findAll().forEach { blocker ->
-            println("Task: ${blocker.content}")
-        }
-    }
 }
